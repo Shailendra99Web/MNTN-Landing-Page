bf7ca7537ce0ee273c2a0a7469a22ca051827830
Shailendra99Web <shailendra99web@gmail.com>
2025-09-19 18:15:00 +0530

    maintained code, etc

--- main.js
+++ main.js
@@ -5,9 +5,47 @@ const $$ = (selector) => document.querySelectorAll(selector);
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // a要素（href="#image1-cloud"）を取得するには、以下のように記述します。
-    const allIndicators = $$('.right-side-nav ul a');
-    const sectionIndicator = $('.section-indicator');
+    // Right side section navigation bar.
+    const rightSideNav = $('.right-side-nav'); // top parent
+    const allIndicators = $$('.right-side-nav ul a'); // all children (nav buttons)
+    const sectionIndicator = $('.section-indicator'); // indicator inside the bar.
+
+    // Function for right side nav bar - appear / disapper (timeout funtion included)
+    const rightSideNavOpacityTimeout = (() => {
+        let _opacity = 1;
+        let timer;
+        return {
+            get opacity() {
+                return _opacity;
+            },
+            set opacity(number) {
+                _opacity: number;
+                if (number === 1) {
+                    rightSideNav.classList.remove('opacity-0');
+                    clearTimeout(timer);
+                    timer = setTimeout(() => {
+                        _opacity = 0
+                        rightSideNav.classList.add('opacity-0');
+                    }, 1700);
+                }
+            }
+        }
+    })()
+    rightSideNavOpacityTimeout.opacity = 1 // To disapper the right side nav bar after rendered.
+    // To apper / disapper the right side nav bar (onhover / onhover out) 
+    rightSideNav.addEventListener('mouseenter', () => {
+        rightSideNavOpacityTimeout.opacity = 1
+    })
+    rightSideNav.addEventListener('mouseleave', () => {
+        rightSideNavOpacityTimeout.opacity = 0
+    })
+    // Same on touch
+    rightSideNav.addEventListener('touchstart', () => {
+        rightSideNavOpacityTimeout.opacity = 1
+    })
+    rightSideNav.addEventListener('touchend', () => {
+        rightSideNavOpacityTimeout.opacity = 0
+    })
 
     // Background images
     const image1Cloud = $('.image1-cloud');
@@ -29,18 +67,20 @@ document.addEventListener('DOMContentLoaded', () => {
         const template = $('.info-card-template');
         const clone = template.content.cloneNode(true);
 
-        clone.querySelector('.info-serial-no').innerText = String(index).padStart(2, '0');
-        clone.querySelector('.subtitle h3').innerText = subtitle;
-        clone.querySelector('.title').innerText = title;
-        clone.querySelector('.description').innerText = description;
-        clone.querySelector('.info-image').src = image;
-        clone.querySelector('.info-card').id = `info-card-${index}`;
+        const clone$ = (selector) => clone.querySelector(selector);
+
+        clone$('.info-serial-no').innerText = String(index).padStart(2, '0');
+        clone$('.subtitle h3').innerText = subtitle;
+        clone$('.title').innerText = title;
+        clone$('.description').innerText = description;
+        clone$('.info-image').src = image;
+        clone$('.info-card').id = `info-card-${index}`;
 
         if (index % 2 === 0) {
-            clone.querySelector('.info-card').classList.add('info-card-flex-row-reverse');
-            clone.querySelector('.info-serial-no').classList.add('info-serial-no-even');
-            clone.querySelector('.half-box-text').classList.add('half-box-text-even');
-            clone.querySelector('.half-box-image').classList.add('half-box-image-even');
+            clone$('.info-card').classList.add('info-card-flex-row-reverse');
+            clone$('.info-serial-no').classList.add('info-serial-no-even');
+            clone$('.half-box-text').classList.add('half-box-text-even');
+            clone$('.half-box-image').classList.add('half-box-image-even');
         }
 
         return clone;
@@ -70,105 +110,51 @@ document.addEventListener('DOMContentLoaded', () => {
         const mainContainer = $('.main-container'); // Main Section.
         layerBackgroundColor.style.height = `${mainContainer.offsetHeight + 200}px` // To set equal height of background color layer to main section height contains after appending all cards.
         // -------------------------
+
+        const observerReducerCardImage = (entry, oddEven) => {
+            const card = entry.target.closest('.info-card');
+            const startDash = card ? card.querySelector('.start-dash') : null;
+            const ratio = entry.intersectionRatio;
+            const top = entry.boundingClientRect.top;
+            if (ratio > 0.6) {
+                const retriveIndex = card.querySelector(`.info-serial-no`).innerText;
+                sectionIndicator.style.transform = `translateY(${retriveIndex}00%)`; // To scroll the right side navbar indicator.
+                rightSideNavOpacityTimeout.opacity = 1; // To disapper the right side navbar.
+            }
+            // To stop the animation / transition of info card.
+            if (top < 0) return;
+            const slideAmount = 50 - ratio * 50;
+            if (oddEven === 'odd') entry.target.style.transform = `translateX(${slideAmount}px)`;
+            else entry.target.style.transform = `translateX(-${slideAmount}px)`;
+            entry.target.style.opacity = 0.3 + ratio * 0.7;
+            startDash.style.maxWidth = `${Math.min((72 * ((ratio * 100) + 30) / 100), 72)}px`
+            // Start opacity at 0.3 (not completely invisible, just slightly faded)
+            // Multiply ratio (0 → 1) by 0.7 → gives us a range 0 → 0.7
+            // Add them together → gives final opacity range 0.3 → 1.0
+        }
+
         // To observer odd card's image and add left-to-right slide-in transition.
         const observerReducerOddCardImage = new IntersectionObserver((entries, observer) => {
-            entries.forEach((entry) => {
-                const card = entry.target.closest('.info-card');
-                const startDash = card ? card.querySelector('.start-dash') : null;
-
-                const ratio = entry.intersectionRatio;
-                const vh = window.innerHeight;
-                const top = entry.boundingClientRect.top;
-
-                if (ratio > 0.6) {
-                    const retriveIndex = card.querySelector(`.info-serial-no`).innerText;
-                    sectionIndicator.style.transform = `translateY(${retriveIndex}00%)`;
-                }
-
-                if (top < 0) {
-                    return;
-                }
-
-                // if (locked != true) {
-                const slideAmount = 50 - ratio * 50;
-                entry.target.style.transform = `translateX(${slideAmount}px)`;
-                entry.target.style.opacity = 0.3 + ratio * 0.7;
-                startDash.style.maxWidth = `${72 * (ratio * 100) / 100}px`// 72px
-                // }
-
-                // Start opacity at 0.3 (not completely invisible, just slightly faded)
-                // Multiply ratio (0 → 1) by 0.7 → gives us a range 0 → 0.7
-                // Add them together → gives final opacity range 0.3 → 1.0
-
-                // if (ratio > 0.7) {
-                //     observer.unobserve(entry.target)
-                //     observer.unobserve(startDash)
-                //     entry.target.style.transform = `translateX(0px)`;
-                //     entry.target.style.opacity = 1;
-                //     startDash.style.maxWidth = `72px`;
-                // }
-            })
+            entries.forEach(entry => observerReducerCardImage(entry, 'odd'))
         }, {
             threshold: thresholds // 0,0.01,0.02,..., 0.99, 1
         });
 
         // To observer even card's image and add right-to-left slide-in transition.
         const observerReducerEvenCardImage = new IntersectionObserver((entries, observer) => {
-            // let locked = false;
-
-            entries.forEach((entry) => {
-                const card = entry.target.closest('.info-card');
-                const startDash = card ? card.querySelector('.start-dash') : null;
-
-                const ratio = entry.intersectionRatio;
-                const top = entry.boundingClientRect.top;
-
-                if (ratio > 0.6) {
-                    const retriveIndex = card.querySelector(`.info-serial-no`).innerText;
-                    sectionIndicator.style.transform = `translateY(${retriveIndex}00%)`;
-                }
-
-                if (top < 0) {
-                    // observer.unobserve(entry.target);
-                    // locked = true;
-                    return;
-                }
-
-                // if (locked != true) {
-                const slideAmount = 50 - ratio * 50;
-                entry.target.style.transform = `translateX(-${slideAmount}px)`;
-                entry.target.style.opacity = 0.3 + ratio * 0.7;
-                // startDash.style.maxWidth = `${Math.min(72, 72 * ratio)}px`
-                startDash.style.maxWidth = `${Math.min((72 * ((ratio * 100) + 30) / 100), 72)}px`
-                // startDash.style.maxWidth = `${72 + (72 * (ratio * 100) / 100)}px`; // 72px
-                // }
-
-                // Start opacity at 0.3 (not completely invisible, just slightly faded)
-                // Multiply ratio (0 → 1) by 0.7 → gives us a range 0 → 0.7
-                // Add them together → gives final opacity range 0.3 → 1.0
-            })
-        }, {
-            threshold: thresholds
-        });
+            entries.forEach(entry => observerReducerCardImage(entry, 'even'))
+        }, { threshold: thresholds });
 
         // To observer card's title, description, read-more button and add bottom-to-up slide-up transition.
         const observerReducerSlide20pxUp = new IntersectionObserver((entries, observer) => {
-            // let locked = false;
-            entries.forEach((entry) => {
+            entries.forEach(entry => {
                 const ratio = entry.intersectionRatio;
                 const top = entry.boundingClientRect.top;
-
                 if (top < 0) {
                     return;
-                    // observer.unobserve(entry.target);
-                    // locked = true;
                 }
-
-                // if (locked != true) {
                 const slideAmount = 20 - (20 * ratio);
                 entry.target.style.transform = `translateY(${slideAmount}px)`;
-                // }
-
             })
         }, {
             threshold: thresholds
@@ -207,18 +193,13 @@ document.addEventListener('DOMContentLoaded', () => {
         entries.forEach((entry) => {
             const ratio = entry.intersectionRatio || 0; // 0〜1
             const top = entry.boundingClientRect.top;
-
             if (ratio > 0.6) {
-                sectionIndicator.style.transform = `translateY(${0}00%)`;
+                sectionIndicator.style.transform = `translateY(${0}00%)`; // To scroll the right side navbar indicator.
+                rightSideNavOpacityTimeout.opacity = 1; // To disapper the right side navbar.
             }
-
             if (top < 0) {
-                // observer.unobserve(entry.target);
-                // locked = true;
                 return;
             }
-
-            // if (locked != true) {
             const slide = 200 * (1 - Math.max(0, Math.min(1, ratio))); // 可視率が低いほど数値↑（0→50）
             const slide2 = 50 * (1 - Math.max(0, Math.min(1, ratio)));
             const slide3 = 20 * (1 - Math.max(0, Math.min(1, ratio)));
@@ -226,7 +207,6 @@ document.addEventListener('DOMContentLoaded', () => {
             image2Mountains.style.transform = `translateY(-${slide2}px)`; // 0→-50px
             bgLayer3BlackWhite.style.transform = `translateY(-${slide2}px)`; // 0→-50px
             image3PersonOnMountain.style.transform = `translateY(${slide3}px)`; // 0→-50px
-            // }
 
         })
     }, {
